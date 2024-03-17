26cb755689be3c5a6542e98c5cbd676c58524f4f
habyarimana caleb <callebhabyar55@gmail.com>
2024-03-18 00:20:06 +0200

    up

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const contactUs = document.getElementById("h");
 const contactInform = document.getElementById("inform");
 const close = document.getElementById("close");
+const toggleButton = document.getElementById("toggle-button");
+const navLink = document.getElementById("navigation-menu");
+const closeBtn = document.getElementById("close-btn");
 
 contactUs.onclick = () => {
   contactInform.style.display = "block";
@@ -9,17 +12,27 @@ contactUs.onclick = () => {
 close.onclick = () => {
   contactInform.style.display = "none";
 };
+closeBtn.onclick = () => {
+  navLink.style.display = "none";
+};
+
 
 document.getElementById("back").onclick = () => {
-    window.location.href == '#home';
+    window.location.href = '/index.html';
 };
 
 // navigation menu responsiveness by toggle button
 
-const toggleButton = document.getElementById("toggle-button");
-const navLinks = document.getElementById("navigation-menu");
+// toggleButton.addEventListener("click", () => {
+//   navLinks.classList.toggle("active-menu");
+//   document.getElementById("home").classList.toggle("home");
+// });
 
-toggleButton.addEventListener("click", () => {
-  navLinks.classList.toggle("active-menu");
-  document.getElementById("home").classList.toggle("home");
-});
+toggleButton.addEventListener('click',toggleNav);
+function toggleNav() {
+  if (navLink.style.display === 'block') {
+    navLink.style.display = 'none';
+  } else {
+    navLink.style.display = 'block';
+  }
+}
