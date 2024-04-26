6c568e4db577183d30dae068b2115d7405d56c89
Your Name <you@example.com>
2024-04-26 23:23:09 +0200

    up

--- main.js
+++ main.js
@@ -36,59 +36,7 @@ document.getElementById('signin').addEventListener('click',function(event){
 // lines of codes for alidating sevices.html
 // lines of codes for alidating sevices.html
 
-document.getElementById('contact-us').addEventListener('submit', function(event){
-event.preventDefault();
 
-let email = document.getElementById('email').value;
-let name = String(document.getElementById('username').value);
-let phone = Number(document.getElementById('phone').value);
-let message = document.getElementById('message').value;
-
-// error tracking
-let errorName = document.getElementById('name-err');
-let errorEmail = document.getElementById('email-err');
-let errorPhone = document.getElementById('phone-err');
-let errorMessages = document.getElementById('msg-err');
-
- let valid = true;
- errorEmail.textContent = "";
- errorMessages.textContent = "";
- errorName.textContent = "";
- errorPhone.textContent = "";
-
- if(!email && !name){
-  errorEmail.textContent = "Please enter your emai ! email is required";
-   errorName.textContent = "Please enter your name";
-  valid = false;
- }
-
- if(!phone && !message){
-  errorPhone.textContent = "Please enter your active phone number";
-  errorMessages.textContent = "Please enter your message here";
-
-  valid = false;
- }
-
- if(valid){
-  alert('Your Information Are Completely Submitted')
-
-  // saving form data to local storage
-
-  let formData = {
-    title: name,
-    content:message,
-    contact:phone,
-    email_Address:email
-  }
-
-  localStorage.setItem('formData',JSON.stringify(formData));
-
-  // reset forrm data
-  document.getElementById('contact-us').reset();
-
- }
-
-})
 
 
 
